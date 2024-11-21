8210223b5aa4c80f77062d1b2effceeb9e1c0098
RonaldYou <ronaldrenyou@gmail.com>
2024-11-20 19:45:16 -0500

    iface

--- zeropilot3.5/include/driver_ifaces/idwg_iface.hpp
+++ zeropilot3.5/include/driver_ifaces/idwg_iface.hpp
@@ -4,12 +4,8 @@
 
 class IIndependentWatchdog{
     protected:
-        IIndependentWatchdog() = default; //do we need this if we don't call it
+        IIndependentWatchdog() = default;
     public:
         virtual ~IIndependentWatchdog() = default;
         virtual refreshWatchdog() = 0;
 }
-
-
-// Do we need file paths when importing hpp files?
-// What is idwg.h for
