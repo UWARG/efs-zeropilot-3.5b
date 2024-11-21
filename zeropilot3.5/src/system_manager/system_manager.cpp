a975ac0afbc1ff4da0382f9866747452305350d5
Raiyan Samin <raiyansamin2@gmail.com>
2024-11-21 18:20:42 -0500

    Added a mutable threshold

--- zeropilot3.5/src/system_manager/system_manager.cpp
+++ zeropilot3.5/src/system_manager/system_manager.cpp
@@ -1,18 +1,18 @@
 #include "system_manager.hpp"
 
-SystemManager::SystemManager(SBusIface *rc_driver, QueueIface<RCMotorControlMessage_t> *queue_driver)
-    : rc_driver_(rc_driver), queue_driver_(queue_driver) {}
+SystemManager::SystemManager(SBusIface *rc_driver, QueueIface<RCMotorControlMessage_t> *queue_driver, int16_t invalid_threshold)
+    : rc_driver_(rc_driver), queue_driver_(queue_driver), invalid_threshold_(invalid_threshold) {}
 
 SystemManager::~SystemManager() {}
 
 void SystemManager::SMUpdate() {
     // TODO: Implement this function
     
-    // If no new data is recieved for 50 ticks, go to failsafe
+    // If no new data is recieved for some time, go to failsafe
     if (!rc_driver_->is_data_new()) {
         invalidRCCount_++;
 
-        if (invalidRCCount_ > 50) {
+        if (invalidRCCount_ > invalid_threshold_) {
             sendDisarmedToAttitudeManager();
         }
     } 
