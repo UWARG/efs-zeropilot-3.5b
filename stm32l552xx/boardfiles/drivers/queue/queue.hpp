d25a3d6f0cedcce58dd6ef11a3b5ff1356d91802
Dami Peng <yasai@mac.home>
2024-12-25 01:57:49 -0500

    add boardfiles iface for msg queue driver

---
+++ stm32l552xx/boardfiles/drivers/queue/queue.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "queue_iface.hpp"
+#include "cmsis_os2.h"
+
+template <typename T>
+class MessageQueue : public IMessageQueue{
+   protected:
+      osMessageQueueId_t *queue_id;
+   public: 
+      MessageQueue(osMessageQueueId_t *queue_id);
+      ~MessageQueue();
+
+      // gets top element of queue
+      T get() override;
+      // pushes template message to the back of the queue
+      T push(T message) override;
+      // pops the top of the queue
+      T pop() override;
+};
