713f7884410c03eebf3854454cae145dec27c0fd
RonaldYou <ronaldrenyou@gmail.com>
2025-01-27 18:21:00 -0500

    updated abstraction to remove dynamic timer calc

--- stm32l552xx/boardfiles/drivers/iwdg/iwdg.hpp
+++ stm32l552xx/boardfiles/drivers/iwdg/iwdg.hpp
@@ -6,13 +6,8 @@
 class IndependentWatchdog : public IIndependentWatchdog{
     private:
 		IWDG_HandleTypeDef* watchdog_;
-		uint32_t prescaler_;
-		uint32_t window_;
-		uint32_t reload_;
-
-		bool counterCalculation(uint32_t timeout, uint32_t &prescaler, uint32_t &counter);
     public:
-        IndependentWatchdog(uint32_t timeout);
+        IndependentWatchdog();
 
         bool refreshWatchdog() override;
 }
