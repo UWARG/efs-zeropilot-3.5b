c7848ee0bf33e25a21454c23035efecac0d86c09
eGuitar999 <ari.vancruyningen@gmail.com>
2025-02-05 23:26:54 -0500

    update after validating with hardware

--- stm32l552xx/boardfiles/drivers/motor/motor.hpp
+++ stm32l552xx/boardfiles/drivers/motor/motor.hpp
@@ -4,11 +4,16 @@
 #include "main.h"
 #include "motor_iface.hpp"
 
-class MotorControl {
+class MotorControl : IMotorControl {
     public:
 		MotorControl(TIM_HandleTypeDef* timer, uint16_t timerChannel);
 
+        /**
+         * @brief Sets the duty cycle as a percent between 5 and 10%
+         * @param percent: number between 1-100
+         */
         void set(uint8_t percent);
+        
     private:
         TIM_HandleTypeDef *timer;
         const uint16_t TIMER_CHANNEL;
@@ -16,24 +21,4 @@ class MotorControl {
     	// set to minimum and max amount of counts in a duty cycle
         const uint32_t MIN_SIGNAL;
         const uint32_t MAX_SIGNAL;
-        const uint16_t DESIRED_FREQUENCY = 50; // PWM frequency in hz
 };
-
-
-/*
-#ifndef MOTOR_CONTROL_HPP
-#define MOTOR_CONTROL_HPP
-
-
-class MotorControl {
-public:
-    virtual ~MotorControl() = default;
-
-    // Set the motor speed
-    virtual void set(uint8_t percent) = 0;
-protected:
-    MotorControl() = default;
-};
-
-#endif // MOTOR_CONTROL_HPP
-*/
