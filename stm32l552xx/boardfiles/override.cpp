ebe38b7617e7c8771ad392fc896c0fee18bc009c
Chandise <c6anders@uwaterloo.ca>
2025-02-09 18:28:17 -0500

    resolve merge conflicts

--- stm32l552xx/boardfiles/override.cpp
+++ stm32l552xx/boardfiles/override.cpp
@@ -23,16 +23,24 @@ int _write(int file, char *ptr, int len)
 void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart)
 {
 	if(huart->Instance == UART4){
+<<<<<<< Updated upstream
 		// insert driver handle
     parse(true);
+=======
+		rcHandle->parse(0); //modify, true/false
+>>>>>>> Stashed changes
 	}
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
 	if(huart->Instance == UART4){
+<<<<<<< Updated upstream
     // insert driver handle
 		parse(false);
+=======
+		rcHandle->parse(1); //modify; true/false
+>>>>>>> Stashed changes
 	}
 }
 
