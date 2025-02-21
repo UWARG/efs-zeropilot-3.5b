1c8634bd9ed5cb11b7b56971b69ee5d0af4fc180
yasaidaisuki <peng.dingkai@gmail.com>
2025-02-21 00:14:37 -0500

    implemented src file, removed pop fnc b/c osMessageQeueGet already pops msg

--- stm32l552xx/boardfiles/drivers/queue/queue.hpp
+++ stm32l552xx/boardfiles/drivers/queue/queue.hpp
@@ -13,9 +13,7 @@ class MessageQueue : public IMessageQueue<T>{
       // gets top element of queue
       T get() override;
       // pushes template message to the back of the queue
-      T push(T message) override;
-      // pops the top of the queue
-      T pop() override;
+      void push(T message) override;
       // counts the # of items in the queue
       int count() override;
       // counts remaining capacity of the queue
--- zeropilot3.5/include/driver_ifaces/queue_iface.hpp
+++ zeropilot3.5/include/driver_ifaces/queue_iface.hpp
@@ -12,10 +12,7 @@ class IMessageQueue {
         virtual T get() = 0;
         
         // pushes template message to the back of the queue
-        virtual T push(T message) = 0;
-        
-        // pops the top of the queue
-        virtual T pop() = 0;
+        virtual void push(T message) = 0;
         
         // counts the # of items in the queue
         virtual int count() = 0;
