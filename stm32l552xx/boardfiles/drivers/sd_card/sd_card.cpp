a19bd4aaa87a51a07b55b5c212f835928ddca17b
eGuitar999 <ari.vancruyningen@gmail.com>
2025-02-26 15:52:44 -0500

    add timestamp and new line to message text in log

--- stm32l552xx/boardfiles/drivers/sd_card/sd_card.cpp
+++ stm32l552xx/boardfiles/drivers/sd_card/sd_card.cpp
@@ -25,9 +25,18 @@ int SDCard::init() {
 
 int SDCard::log(char* message) {
     FRESULT res;
+    char msgToSend[112]; //10 for timestamp, 100 for message, 2 for new line
+    int tsStrLen;
+
+    uint32_t ts = (uint32_t)(osKernelGetTickCount() * 1.0 / osKernelGetTickFreq());
+    tsStrLen = snprintf(msgToSend, 10, "%lus: ", ts);
 
     res = f_open(&fil, file, FA_WRITE | FA_OPEN_APPEND);
-    res = (FRESULT)f_puts(message, &fil);
+
+    snprintf(msgToSend + tsStrLen, 100, message);
+    snprintf(msgToSend + tsStrLen + strlen(message), 3, "\r\n");
+    res = (FRESULT)f_puts(msgToSend, &fil);
+
     res = f_close(&fil);
 
     return res;
@@ -35,11 +44,18 @@ int SDCard::log(char* message) {
 
 int SDCard::log(char message[][100], int count) {
     FRESULT res;
+    char msgToSend[112]; //10 for timestamp, 100 for message, 2 for new line
+    int tsStrLen;
+
+    uint32_t ts = (uint32_t)(osKernelGetTickCount() * 1.0 / osKernelGetTickFreq());
+    tsStrLen = snprintf(msgToSend, 10, "%lus: ", ts);
 
     res = f_open(&fil, file, FA_WRITE | FA_OPEN_APPEND);
 
     for (int i = 0; i < count; i++) {
-      res = (FRESULT)f_puts(message[i], &fil);
+      snprintf(msgToSend + tsStrLen, 100, message[i]);
+      snprintf(msgToSend + tsStrLen + strlen(message[i]), 3, "\r\n");
+      res = (FRESULT)f_puts(msgToSend, &fil);
     }
 
     res = f_close(&fil);
