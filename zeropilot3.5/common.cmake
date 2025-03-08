244ea123da4e7cb694fe50d6b64f24a070b9e0b1
dechair3 <dechair3@gmail.com>
2025-03-08 16:16:08 -0500

    Initial Commit

--- zeropilot3.5/common.cmake
+++ zeropilot3.5/common.cmake
@@ -14,13 +14,23 @@ set(SM_INC
 
 )
 
+# telemetry manager files
+set(TM_SRC
+
+)
+set(TM_INC
+    "include/telemetry_manager"
+)
+
 # combined files
 set(ZP_SRC
     ${AM_SRC}
     ${SM_SRC}
+    ${TM_SRC}
 )
 set(ZP_INC
     "include/driver_ifaces"
     ${AM_INC}
     ${SM_INC}
+    ${TM_INC}
 )
