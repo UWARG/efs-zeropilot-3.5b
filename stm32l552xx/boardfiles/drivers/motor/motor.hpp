f3bce2557bd443e4c811833b8235c1fbfcde09b0
Bill Lu <80836399+William-Lu-Jia-Rong@users.noreply.github.com>
2025-03-09 16:52:23 -0400

    Update motor.hpp

--- stm32l552xx/boardfiles/drivers/motor/motor.hpp
+++ stm32l552xx/boardfiles/drivers/motor/motor.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 #include "motor_iface.hpp"
-#include "stm32l5xx_hal_tim.h"
+#include "main.h"
 
-class MotorControl : IMotorControl {
+class MotorControl{
     public:
         MotorControl(TIM_HandleTypeDef* timer, uint16_t timerChannel);
 
@@ -12,12 +12,14 @@ class MotorControl : IMotorControl {
          * @param percent: number between 1-100
          */
         void set(uint8_t percent);
-        
+        bool setDutyCycleLimits(uint8_t minPercentage, uint8_t maxPercentage);
+        void start();
     private:
-        TIM_HandleTypeDef *timer;
+        TIM_HandleTypeDef *const timer;
         const uint16_t TIMER_CHANNEL;
 
     	// set to minimum and max amount of counts in a duty cycle
-        const uint32_t MIN_SIGNAL;
-        const uint32_t MAX_SIGNAL;
+        uint32_t MIN_SIGNAL;
+        uint32_t MAX_SIGNAL;
+        const uint16_t DESIRED_FREQUENCY = 50;
 };
