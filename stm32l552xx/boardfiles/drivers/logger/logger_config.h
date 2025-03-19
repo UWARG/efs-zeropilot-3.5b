7843927037da9255158656cb32e519a5b4573ab3
Derek Tang <d6tang@uwaterloo.ca>
2025-03-19 12:41:04 -0400

    refactor logger, move files around

--- stm32l552xx/boardfiles/drivers/logger/logger_config.h
+++ stm32l552xx/boardfiles/drivers/logger/logger_config.h
@@ -1,20 +1,10 @@
-/*
- * config.h
- *
- *  Created on: Mar 3, 2025
- *      Author: arigu
- */
-
-#ifndef INC_CONFIG_H_
-#define INC_CONFIG_H_
+#pragma once
 
 #define SD_CARD_LOGGING
-//#define SWO_LOGGING
+// #define SWO_LOGGING
 
 #if defined(SD_CARD_LOGGING) && defined(SWO_LOGGING)
   #error Only one can be defined
 #elif !defined(SD_CARD_LOGGING) && !defined(SWO_LOGGING)
   #error Define a Logging interface
 #endif
-
-#endif /* INC_CONFIG_H_ */
