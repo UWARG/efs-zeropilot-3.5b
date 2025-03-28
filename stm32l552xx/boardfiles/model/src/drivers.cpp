cef82dac2e3d7b8a6cae1a1577d4fe9eba48bd5b
eGuitar999 <ari.vancruyningen@gmail.com>
2025-03-28 12:39:29 -0400

    added drivers to initDrivers

--- stm32l552xx/boardfiles/model/src/drivers.cpp
+++ stm32l552xx/boardfiles/model/src/drivers.cpp
@@ -4,7 +4,22 @@ extern UART_HandleTypeDef huart4;
 
 RCReceiver *rcHandle = nullptr;
 
+#include "iwdg.hpp"
+#include "logger.hpp"
+#include "motor.hpp"
+
+#include "stm32l5xx_hal.h"
+
+extern IWDG_HandleTypeDef hiwdg;
+extern TIM_HandleTypeDef htim2;
+
+IndependentWatchdog *iwdgHandle = nullptr;
+Logger *loggerHandle = nullptr;
+MotorControl *motorHandle = nullptr;
+
 void initDrivers()
 {
-    rcHandle = new RCReceiver(&huart4); 
+    rcHandle = new RCReceiver(&huart4);     iwdgHandle = new IndependentWatchdog(&hiwdg);
+    loggerHandle = new Logger();
+    motorHandle = new MotorControl(&htim2, TIMER_CHANNEL_1, 5, 10);
 }
