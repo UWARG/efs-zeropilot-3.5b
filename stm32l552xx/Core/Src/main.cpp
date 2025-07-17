97168f447118952df4f878ee3f658a306b47c2b0
Ronak <98669069+Rosnaky@users.noreply.github.com>
2025-07-17 20:24:29 +0000

    reorder attributes

--- stm32l552xx/Core/Src/main.cpp
+++ stm32l552xx/Core/Src/main.cpp
@@ -68,8 +68,8 @@ DMA_HandleTypeDef hdma_usart2_rx;
 osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
   .name = "defaultTask",
-  .priority = (osPriority_t) osPriorityNormal,
   .stack_size = 128 * 4
+  .priority = (osPriority_t) osPriorityNormal,
 };
 /* USER CODE BEGIN PV */
 
