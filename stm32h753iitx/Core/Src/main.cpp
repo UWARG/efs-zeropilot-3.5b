bce9cccec79b031eae57a7c6e873e74c5ff22891
Ronak Patel <98669069+Rosnaky@users.noreply.github.com>
2025-11-28 17:05:10 -0500

    volatile buffer

--- stm32h753iitx/Core/Src/main.cpp
+++ stm32h753iitx/Core/Src/main.cpp
@@ -71,12 +71,12 @@ UART_HandleTypeDef huart6;
 DMA_HandleTypeDef hdma_uart4_rx;
 
 /* Definitions for defaultTask */
-osThreadId_t defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityNormal,
-};
+//osThreadId_t defaultTaskHandle;
+//const osThreadAttr_t defaultTask_attributes = {
+//  .name = "defaultTask",
+//  .stack_size = 128 * 4,
+//  .priority = (osPriority_t) osPriorityNormal,
+//};
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
