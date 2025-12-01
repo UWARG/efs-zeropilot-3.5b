93ed634dd1c5cc50dc095803ff86cd42647fed59
Aditya Bang <adityabang15@gmail.com>
2025-11-30 20:54:34 -0500

    roll was correct first time

--- zeropilot4.0/src/attitude_manager/MahonyAHRS.cpp
+++ zeropilot4.0/src/attitude_manager/MahonyAHRS.cpp
@@ -128,7 +128,7 @@ void Mahony::updateIMU(float gx, float gy, float gz, float ax, float ay, float a
 	q2 *= recipNorm;
 	q3 *= recipNorm;
 
-	roll = -atan2f(q0*q1 + q2*q3, 0.5f - q1*q1 - q2*q2);
+	roll = atan2f(q0*q1 + q2*q3, 0.5f - q1*q1 - q2*q2);
 	pitch = asinf(-2.0f * (q1*q3 - q0*q2));
 	yaw = atan2f(q1*q2 + q0*q3, 0.5f - q2*q2 - q3*q3);
 }
