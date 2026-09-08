67ec86dbc8ea289d2ce866090b416ed792031e10
Ronak Patel <98669069+Rosnaky@users.noreply.github.com>
2026-09-08 04:22:08 -0400

    Clean up model.cpp

--- stm32h753iitx/boardfiles/model/src/model.cpp
+++ stm32h753iitx/boardfiles/model/src/model.cpp
@@ -1,20 +1,17 @@
 #include "drivers.hpp"
 #include "managers.hpp"
+#include "model.hpp"
 #include "zp_params.hpp"
 #include "zp_bit.hpp"
 
 void initModel()
 {
-  // SystemUtils first: BIT needs its millisecond clock before any driver reports into it.
   initSystemUtils();
   (void)ZP_BIT::init(systemUtilsHandle);
 
-  // Params before drivers: initDrivers reads the servo table while constructing motor handles
-  ZP_Error paramStatus = ZP_PARAM::init();
+  (void)ZP_BIT::report(ZP_BIT_ID::PARAM_TABLE_INIT, ZP_PARAM::init());
 
   initDrivers();
-
-  (void)ZP_BIT::report(ZP_BIT_ID::PARAM_TABLE_INIT, paramStatus);
 
   initManagers();
 }
--- stm32l552xx/boardfiles/model/src/model.cpp
+++ stm32l552xx/boardfiles/model/src/model.cpp
@@ -1,22 +1,17 @@
-#include "model.hpp" 
 #include "drivers.hpp"
 #include "managers.hpp"
+#include "model.hpp"
 #include "zp_params.hpp"
 #include "zp_bit.hpp"
-#include "zp_error.h"
 
 void initModel()
 {
-  // SystemUtils first: BIT needs its millisecond clock before any driver reports into it.
   initSystemUtils();
   (void)ZP_BIT::init(systemUtilsHandle);
 
-  // Params before drivers: initDrivers reads the servo table while constructing motor handles
-  ZP_Error paramStatus = ZP_PARAM::init();
+  (void)ZP_BIT::report(ZP_BIT_ID::PARAM_TABLE_INIT, ZP_PARAM::init());
 
   initDrivers();
-
-  (void)ZP_BIT::report(ZP_BIT_ID::PARAM_TABLE_INIT, paramStatus);
 
   initManagers();
 }
